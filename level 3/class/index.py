912b7edf36c28aa60fe164162db1fb71db023764
GEORGEafold <kebadzegio383@gmail.com>
2024-11-16 18:48:09 +0400

    first commit

---
+++ level 3/class/index.py
@@ -0,0 +1,15 @@
+#ცვლადი შდგება:
+#1) ცვლადის სახელი
+#2) დამაკავშირებელი სიმბოლო(=)
+#3) ცვლადის მნიშნელობა
+
+#name = 'giorgi'
+#last_name = ' kebadze'
+#country = ' georgia'
+
+#print('Hello! my name is ' + name +'. ' + 'my last name is ' + last_name + '. ' 'i am from' + country + '.')
+
+x = 15
+y = 16
+z = x + y
+print(z)
