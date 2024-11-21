6da24e57b287dbe33f60333f94918c353a94762e
GEORGEafold <kebadzegio383@gmail.com>
2024-11-21 18:11:12 +0400

    second commit

---
+++ level 01/class/index.py
@@ -0,0 +1,3 @@
+name = "Gio"
+age = "16"
+print("my name is " + name + " and i am " + age + " years old")
---
+++ level 03/home/index.py
@@ -0,0 +1,15 @@
+x = 5 #pirveli cvladi
+y = 6 #meore cvladi
+print(x + y) # am ori cvladis jams vprintavt
+
+num1 = 17
+num2 = 24
+num3 = 2
+
+print(num1 + num2 + num3)
+
+
+
+length = 20
+width = 10
+print(length + width)
