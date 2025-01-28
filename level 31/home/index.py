f3a331ef8a726661f6cff7cd46bcbc2e593576b7
GEORGEafold <kebadzegio383@gmail.com>
2025-01-28 22:14:34 +0400

    second commit

---
+++ level 31/home/index.py
@@ -0,0 +1,15 @@
+#1)
+
+
+#x = "Hello,{name}. WElcome to {place}."
+#x.format(name = "Alice")
+#x.format(place = "Wonderland")
+#formatted_string = x
+#print(formatted_string)
+
+
+#2)
+
+
+#words = ["apple", "banana", "cherry"]
+#fruit_string = words.join()
